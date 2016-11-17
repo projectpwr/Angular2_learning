239f8f422ebbf9ba0fd9c761a01fee1015624453
Will Young <willyoung0@googlemail.com>
2016-11-17 21:28:46 +0000

    thrown in bootstrap template for later on. making start with components on tour of heros tutorial

---
+++ app/ts/album.ts
@@ -0,0 +1,11 @@
+@class album {
+  id:integer;
+  name:string;
+  type:string;
+
+  constructor(id:integer,name:string,type:string){
+    this.id = id;
+    this.name = name;
+    this.type = type;
+  }
+}
--- systemjs.config.js
+++ systemjs.config.js
@@ -32,7 +32,7 @@
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
-        main: './main.js',
+        main: './js/app/ts/main',
         defaultExtension: 'js'
       },
       rxjs: {
